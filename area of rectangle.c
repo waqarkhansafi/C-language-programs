0e264bbd2e92966bef9f136850bb145ef1420bcf
WaQar khan safi <58323408+waqarkhansafi@users.noreply.github.com>
2020-02-10 23:38:47 +0500

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+int w,h,area;
+printf("enter the value of width ");
+scanf("%d",& w);
+printf("enter the value of height ");
+scanf("%d",& h);
+area=w*h;
+printf("the total area of rectangle is %d", area);
+}
