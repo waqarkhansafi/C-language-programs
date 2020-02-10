bd39fde529fc72899223429e2641f3cff5482206
WaQar khan safi <58323408+waqarkhansafi@users.noreply.github.com>
2020-02-10 23:43:02 +0500

    Add files via upload

---
+++ array.4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,arr[10],sum=0,max,min;
+for(a=0;a<=9;a++)
+{
+printf("Enter the array number ");
+scanf("%d",& arr[a]);
+}
+for(a=9;a>=0;a--)
+printf("\nThe number of array is %d", arr[a]);
+for(a=0;a<=9;a++)
+sum=sum+arr[a];
+printf("\nThe sum of array is %d", sum);
+max=arr[0];
+for(a=1;a<=9;a++)
+    if(max<arr[a])
+    max=arr[a];
+printf("\nThe maximum number of array is %d", max);
+min=arr[0];
+for(a=0;a<=9;a++)
+    if(min>arr[a])
+ min=arr[a];
+printf("\nThe minimum number of array is %d", min);
+}
