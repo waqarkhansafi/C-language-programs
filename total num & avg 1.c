fb913e4b2185f813dfcfc907f7ad2106b3ac5aed
WaQar khan safi <58323408+waqarkhansafi@users.noreply.github.com>
2020-07-09 13:42:56 +0500

    Add files via upload

---
+++ total num & avg 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int phy,math,pro,ict,eng,isl,sum;
+phy=33;
+math=8;
+pro=18;
+ict=21;
+eng=30;
+isl=27;
+printf("\n The total number of papers is %d", sum=phy+math+pro+ict+eng+isl);
+printf("\n Average of total num is %d", sum/6);
+}
